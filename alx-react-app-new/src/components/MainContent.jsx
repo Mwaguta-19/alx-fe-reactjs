1bc7825c0348d1052f1bc92915577c2444d646c3
Mwaguta <mwatsenzemwaguta14@gmail.com>
2025-02-23 10:27:31 +0300

    inlinecss in react

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+function MainContent(){
+    return (
+        <main style={{fontSize: '20px', fontFamily:'sans-serif', color:"blue"}}>
+            <p>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+
+}
+
+export default MainContent
+
