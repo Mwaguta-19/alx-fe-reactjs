13becd50a044a55f187c1c2e29c62b02c6e81c2a
Mwaguta <mwatsenzemwaguta14@gmail.com>
2025-02-23 23:14:54 +0300

    props implementation

--- alx-react-app-props/src/components/App.jsx
+++ alx-react-app-props/src/components/App.jsx
@@ -6,7 +6,7 @@ function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
-    <UserContext.Provider value= {useData}>
+    <UserContext.Provider value= {userData}>
         <ProfilePage/>
     </UserContext.Provider>
   )
