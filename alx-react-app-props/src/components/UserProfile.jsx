eeb760c9f70e682fb10fdd1eac2972f2866f6399
Mwaguta <mwatsenzemwaguta14@gmail.com>
2025-02-23 23:33:23 +0300

    props implementation

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,11 +1,20 @@
-const UserProfile = (props) => {
-    return (
-      <div>
-        <h2>{props.name}</h2>
-        <p>Age: {props.age}</p>
-        <p>Bio: {props.bio}</p>
-      </div>
-    );
-  };
+import React, { useContext } from 'react'; 
+import UserContext from '../UserContext'; 
 
-  export default UserProfile;
+
+
+
+function UserProfile() {
+  // Use the useContext hook to consume the context
+  const userData = useContext(UserContext);
+
+  return (
+    <div>
+      <h2>User Profile</h2>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
+}
+
+export default UserProfile;
