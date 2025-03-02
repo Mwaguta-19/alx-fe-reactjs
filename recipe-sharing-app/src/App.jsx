954594d7842fcec5762e5b38eb666a0ee34408c0
Mwaguta <mwatsenzemwaguta14@gmail.com>
2025-03-02 22:28:38 +0300

    filesreact-router dom set

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
-import { RecepeList } from './components/RecipeList'
+import { RecipeList } from './components/RecipeList';
 //import { RecipeList } from './components/RecipeList';
-import { AddRecipeForm }  from './components/AddRecipeForm'
+import { AddRecipeForm }  from './components/AddRecipeForm';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { RecipeDetails } from './components/RecipeDetails';
 //import { AddRecipeForm } from './components/AddRecipeForm';
 
 function App() {
@@ -9,7 +11,16 @@ function App() {
     <div className="App">
       <h1>Recipe Sharing App</h1>
       <AddRecipeForm />
-      <RecipeList />
+      <Router>
+      <div className="App">
+        <h1>Recipe Sharing App</h1>
+        <AddRecipeForm />
+        <Switch>
+          <Route path="/" exact component={RecipeList} />
+          <Route path="/recipe/:id" component={RecipeDetails} />
+        </Switch>
+      </div>
+    </Router>
     </div>
   );
 }
---
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useRecipeStore } from '../recipeStore';
+import { useHistory } from 'react-router-dom';
+import { EditRecipeForm } from './EditRecipeForm';
+import { DeleteRecipeButton } from './DeleteRecipeButton';
+
+const RecipeDetails = ({ match }) => {
+  const { id } = match.params;  // Retrieve the recipe ID from the URL
+  const recipe = useRecipeStore(state => state.recipes.find(recipe => recipe.id === parseInt(id)));
+
+  if (!recipe) {
+    return <div>Recipe not found!</div>;
+  }
+
+  return (
+    <div>
+      <h1>{recipe.title}</h1>
+      <p>{recipe.description}</p>
+
+      {/* Render Edit and Delete components */}
+      <EditRecipeForm recipe={recipe} />
+      <DeleteRecipeButton recipeId={recipe.id} />
+    </div>
+  );
+};
+
+export { RecipeDetails };
