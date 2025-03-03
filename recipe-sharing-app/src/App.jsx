7111a4ec52c93e3f0f9800b1663d46cd726bb266
Mwaguta <mwatsenzemwaguta14@gmail.com>
2025-03-03 09:33:05 +0300

    filesreact-router dom set

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import Router and Routes from react-router-dom v6
 import { RecipeList } from './components/RecipeList';
-import { AddRecipeForm } from './components/AddRecipeForm';
-import { RecipeDetails } from './components/RecipeDetails'; // Ensure you import the RecipeDetails component
+import AddRecipeForm from './components/AddRecipeForm';
+import RecipeDetails from './components/RecipeDetails'; // Ensure you import the RecipeDetails component
 
 function App() {
   return (
