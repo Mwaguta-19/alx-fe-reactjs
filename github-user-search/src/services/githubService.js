f325beca1f8d62c2682f83f4bf8c5b31a9f7020e
Mwaguta <mwatsenzemwaguta14@gmail.com>
2025-03-30 23:47:08 +0300

    Integrating the GitHub API and Creating a Basic Search Feature

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,6 +1,25 @@
 import axios from 'axios';
 
-export const fetchUserData = async (username) => {
-  const response = await axios.get(`https://api.github.com/users/${username}`);
-  return response.data; // Return the user data
+// Function to fetch user data from GitHub API with advanced filters
+export const fetchUserData = async (username, location, minRepos) => {
+  const query = buildQuery(username, location, minRepos);
+  
+  const response = await axios.get(`https://api.github.com/search/users?q=${query}`);
+  
+  return response.data; // Return the list of users matching the query
+};
+
+// Function to construct the query string
+const buildQuery = (username, location, minRepos) => {
+  let query = `in:login ${username}`;
+  
+  if (location) {
+    query += ` location:${location}`;
+  }
+  
+  if (minRepos) {
+    query += ` repos:>=${minRepos}`;
+  }
+  
+  return query;
 };
