2458a3eeb4005b563c5071fccebcb7eb10df5097
Mwaguta <mwatsenzemwaguta14@gmail.com>
2025-04-14 18:12:18 +0300

    enhancing the recipeapp

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,18 +1,14 @@
-import React from 'react';
-import { useRecipeStore } from '../recipeStore';
+import React from "react";
+import { useRecipeStore } from "./recipeStore";
 
 const DeleteRecipeButton = ({ recipeId }) => {
-  const deleteRecipe = useRecipeStore(state => state.deleteRecipe);
+  const deleteRecipe = useRecipeStore((state) => state.deleteRecipe);
 
   const handleDelete = () => {
     deleteRecipe(recipeId);
   };
 
-  return (
-    <button onClick={handleDelete}>
-      Delete Recipe
-    </button>
-  );
+  return <button onClick={handleDelete}>Delete Recipe</button>;
 };
 
-export { DeleteRecipeButton };
+export default DeleteRecipeButton;
--- recipe-sharing-app/src/components/RecipeDetails.jsx
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -1,27 +1,23 @@
-import React from 'react';
-import { useRecipeStore } from '../recipeStore';
-import { useNavigate } from 'react-router-dom';
-import { EditRecipeForm } from './EditRecipeForm';
-import { DeleteRecipeButton } from './DeleteRecipeButton';
+import React from "react";
+import { useRecipeStore } from "./recipeStore";
+import { useNavigate } from "react-router-dom";
+import { EditRecipeForm } from "./EditRecipeForm";
+import { DeleteRecipeButton } from "./DeleteRecipeButton";
 
-const RecipeDetails = ({ match }) => {
-  const { id } = match.params;  // Retrieve the recipe ID from the URL
-  const recipe = useRecipeStore(state => state.recipes.find(recipe => recipe.id === parseInt(id)));
-
-  if (!recipe) {
-    return <div>Recipe not found!</div>;
-  }
+const RecipeDetails = ({ recipeId }) => {
+  const recipe = useRecipeStore((state) =>
+    state.recipes.find((recipe) => recipe.id === recipeId)
+  );
 
   return (
     <div>
       <h1>{recipe.title}</h1>
       <p>{recipe.description}</p>
-
-      {/* Render Edit and Delete components */}
+      {/* Render EditRecipeForm and DeleteRecipeButton here */}
       <EditRecipeForm recipe={recipe} />
       <DeleteRecipeButton recipeId={recipe.id} />
     </div>
   );
 };
 
-export { RecipeDetails };
+export default RecipeDetails;
