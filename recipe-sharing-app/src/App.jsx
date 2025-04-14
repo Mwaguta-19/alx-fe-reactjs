7cd48c73465c5d76726daa8e5ccf05c4f3ea8a69
Mwaguta <mwatsenzemwaguta14@gmail.com>
2025-04-14 16:59:52 +0300

    zustand store added

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import Router and Routes from react-router-dom v6
-import { RecipeList } from './components/RecipeList';
-import AddRecipeForm from './components/AddRecipeForm';
-import RecipeDetails from './components/RecipeDetails'; // Ensure you import the RecipeDetails component
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Import Router and Routes from react-router-dom v6
+import RecipeList from "./components/RecipeList";
+import AddRecipeForm from "./components/AddRecipeForm";
+import RecipeDetails from "./components/RecipeDetails"; // Ensure you import the RecipeDetails component
 
 function App() {
   return (
@@ -11,7 +11,8 @@ function App() {
         <h1>Recipe Sharing App</h1>
         <Routes>
           <Route path="/" element={<RecipeList />} />
-          <Route path="/recipe/:id" element={<RecipeDetails />} /> {/* Setup route for individual recipe */}
+          <Route path="/recipe/:id" element={<RecipeDetails />} />{" "}
+          {/* Setup route for individual recipe */}
           <Route path="/add" element={<AddRecipeForm />} />
         </Routes>
       </div>
